50879413c358929354d52c04f1e1494c602a28cf
Sohrab <mdsohrabhossainsohel902833@gmail.com>
2020-10-31 12:43:43 +0600

    sohrab

---
+++ src/main/java/com/example/sohellibrary/Sohel.java
@@ -0,0 +1,15 @@
+package com.example.sohellibrary;
+
+import android.util.Log;
+
+import java.util.logging.Logger;
+
+public class Sohel {
+
+    public static  void  save(String value){
+        Log.d("ke",value);
+
+    }
+
+
+}
